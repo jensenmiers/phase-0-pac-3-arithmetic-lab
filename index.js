4285f2704c2bce2cdf2b2370e1c6f2c8daff1814
jensenmiers <jensenmiers@gmail.com>
2022-12-04 12:43:52 -0800

    commit tree 6f309e108cf1ed06278766cc57716aa48b053bf8

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x, y) {
+    return x + y
+}
+
+function subtract(b,a) {
+    return b - a
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
